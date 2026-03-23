913d4c9d5c5eb6f1a8a612fab1ff89dc02d4f6a7
Aliciacoco <aliciakccoco@gmail.com>
2026-03-23 10:05:20 +0800

    update

--- resources.js
+++ resources.js
@@ -1,20 +1,41 @@
+
 // resources.js
 const libraryData = [
-    // {
-    //     title: "2026 战略规划蓝图.pdf",
-    //     type: "PDF",
-    //     size: "1.2 MB",
-    //     link: "https://your-aliyun-link.com/file1.pdf",
-    //     color: "#FF4B4B", // 红色
-    //     shadow: "#D33131"
-    // },
-    // {
-    //     title: "coco-os 使用指南.pdf",
-    //     type: "DOC",
-    //     size: "850 KB",
-    //     link: "https://your-aliyun-link.com/file2.pdf",
-    //     color: "#58CC02", // 绿色
-    //     shadow: "#4CAF00"
-    // }
-    // 以后想加新文件，直接复制粘贴上面的一段即可
+    {
+        title: "关于CNN留学中国预科教育联盟",
+        type: "LINK",
+        size: "",
+        info: "高中嵌入式预科", // ✨ 在这里自定义每行的小字
+        link: "https://docs.qq.com/doc/DZE1Yb2R6d2xaSG9i",
+        color: "#FF4B4B", // 红色
+        shadow: "#D33131"
+    },
+    {
+        title: "关于CampusMate",
+        type: "LINK",
+        size: "",
+        info: "AI小智 + 校园大使答疑", // ✨ 在这里自定义每行的小字
+        link: "https://docs.qq.com/doc/DZFNaWnVmdnpkVW10",
+        color: "#FF4B4B", // 红色
+        shadow: "#D33131"
+    },
+    {
+        title: "关于LinkTree",
+        type: "LINK",
+        size: "",
+        info: "国外社媒 链接索引聚合页", // ✨ 在这里自定义每行的小字
+        link: "https://docs.qq.com/doc/DZFZzdmNVU05RdEF1",
+        color: "#FF4B4B", // 红色
+        shadow: "#D33131"
+    },
+];
+
+// resources.js
+const projectData = [
+    {
+        title: "AI公众号运营助手",
+        desc: "选题 + 生成文章 + 自动发布",
+        link: "https://ai.coco-os.cn/", // ✨ 填入你的项目在线预览地址
+        color: "#FF9600"
+    },
 ];
